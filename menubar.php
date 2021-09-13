ca7f027c8196e38eee25868823fa2d573e05023f
Rupesh :) <rupeshnarkar500@gmail.com>
2021-09-13 20:01:42 +0530

    Update menubar.php
    menubar update changes in top menubar

--- menubar.php
+++ menubar.php
@@ -2,21 +2,6 @@
         
       
 
-<!-- top navigation start -->
-<div class="topnav">
-<a href="https://twitter.com/login?lang=en" class="nav-link" ><i class="fab fa-twitter"></i></a>
-<a href="https://www.facebook.com/" class="nav-link" ><i class="fab fa-facebook-f"></i></a>
-<a href="#" class="nav-link" ><i class="fab fa-pinterest-p"></i></a>
-<a href="mailto:thelaundary360@support.com" class="nav-link ml-5" ><i class="fa fa-envelope"></i> thelaundary360@support.com</a>
-<a href="#" class="nav-link" ><i class="far fa-clock"></i>  Mon-Fri 08:00 AM - 05:00 PM</a>
-
-<a href="#" class="nav-link user" > <i class="fa fa-user-circle" aria-hidden="true"></i></a>
-
-<a href="tel:(+91) 987 654 3210" class="nav-link  phone" href="index.php"   ><i class="fa fa-phone-square text-primary" aria-hidden="true"></i>  (+91) 987 654 3210</a>  
-
-</div>
-
-<!-- main header start -->
 
         <div class="header-area header">
             <div class="main-header header-sticky">

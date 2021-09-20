590b118f8fa30c06fc756f860a10088c6cdc5417
Rupesh :) <rupeshnarkar500@gmail.com>
2021-09-20 22:55:20 +0530

    creating login page and design it
    design login page final

--- login.php
+++ login.php
@@ -1,82 +1,191 @@
-<!DOCTYPE html>
-<html lang="en">
-   <head>
-      <meta charset="utf-8" />
-      <title>Log in | thelaundary360</title>
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-      <link rel="shortcut icon" href="assets/images/favicon/ ">
-      <link href="assets/css/bootstrap.min.css" id="bootstrap-stylesheet" rel="stylesheet" type="text/css" />
-      <link rel="stylesheet" href="assets/css/register.css">
-      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha384-wvfXpqpZZVQGK6TAh4PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEh4Sl0sibVcOQVnN" crossorigin="anonymous">
-   </head>
-   <body class="authentication-bg">
-      <div class="account-pages mt-5 mb-5">
-         <div class="container">
-            <div class="row justify-content-center">
-               <div class="col-md-8 col-lg-6 col-xl-5">
-                  <div class="log">
-                     <img src="assets/img/logo/logo.png"alt="">
-                  </div>
-                  <div class="card">
-                     <div class="card-body ">
-                        <div class="header text-center mb-4">
-                           <h4 class="text-uppercase mt-0">SIGN IN</h4>
-                        </div>
-                        <form action="#">
-                           <div class="form-group mb-3">
-                              <label for="mobile">Mobile</label>
-                              <input class="form-control" type="text" id="mobilenum" required="" placeholder="mobile number">
-                           </div>
-                           <div class="form-group mb-3">
-                              <label for="password">Password</label>
-                              <input class="form-control" type="password" required="" id="password" placeholder="password">
-                           </div>
-                           <div class="form-group mb-3">
-                              <div class="custom-control custom-checkbox">
-                                 <input type="checkbox" class="custom-control-input" id="checkbox-signin" checked>
-                                 <label class="custom-control-label" for="checkbox-signin">Remember me</label>
-                              </div>
-                           </div>
-                           <div class="form-group mb-2 text-center">
-                              <button class="btn btn-primary btn-block" type="submit"> Log In </button>
-                           </div>
-                           <a href="forgot-password.php" class="gray pt-5"><i class="fa fa-lock" aria-hidden="true"></i> Forgot Your Password?</a>
-                        </form>
-                     </div>
-                  </div>
-                  <div class="mt-3 text-center">
-                     <p class="gray">Don't have an account?<a href="register.php" class="no-border text-primary font-weight-bold">Create an Account</a></p>
-                  </div>
-               </div>
-            </div>
-         </div>
-      </div>
 
-      <script src="assets/js/vendor/modernizr-3.5.0.min.js"></script>
-      <script src="assets/js/vendor/jquery-1.12.4.min.js"></script>
-      <script src="assets/js/popper.min.js"></script>
-      <script src="assets/js/bootstrap.min.js"></script>
-      <script src="assets/js/jquery.slicknav.min.js"></script>
-      <script src="assets/js/owl.carousel.min.js"></script>
-      <script src="assets/js/slick.min.js"></script>
-      <script src="assets/js/wow.min.js"></script>
-      <script src="assets/js/animated.headline.js"></script>
-      <script src="assets/js/jquery.magnific-popup.js"></script>
-      <script src="assets/js/gijgo.min.js"></script>
-      <script src="assets/js/jquery.nice-select.min.js"></script>
-      <script src="assets/js/jquery.sticky.js"></script>
-      <script src="assets/js/jquery.barfiller.js"></script>
-      <script src="assets/js/jquery.counterup.min.js"></script>
-      <script src="assets/js/waypoints.min.js"></script>
-      <script src="assets/js/jquery.countdown.min.js"></script>
-      <script src="assets/js/hover-direction-snake.min.js"></script>
-      <script src="assets/js/contact.js"></script>
-      <script src="assets/js/jquery.form.js"></script>
-      <script src="assets/js/jquery.validate.min.js"></script>
-      <script src="assets/js/mail-script.js"></script>
-      <script src="assets/js/jquery.ajaxchimp.min.js"></script>
-      <script src="assets/js/plugins.js"></script>
-      <script src="assets/js/main.js"></script>
-   </body>
-   
+<html lang="en">
+
+
+<head>
+  <title>Thelaundry360</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+<!--
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+-->
+    
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+    
+</head>
+<style>
+/*USER LOGIN PAGE CSS*/
+    
+    @import url("//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css");
+.login-block {
+	background: #DE6262;
+	/* fallback for old browsers */
+	background: -webkit-linear-gradient(to bottom, #FFB88C, #DE6262);
+	/* Chrome 10-25, Safari 5.1-6 */
+	background: linear-gradient(to bottom, #FFB88C, #DE6262);
+	/* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+	float: left;
+	width: 100%;
+    height: 100vh;
+	padding: 50px 0;
+}
+
+.banner-sec {
+	background: url(https://static.pexels.com/photos/33972/pexels-photo.jpg) no-repeat left bottom;
+	background-size: cover;
+	min-height: 500px;
+	border-radius: 0 10px 10px 0;
+	padding: 0;
+}
+
+.container {
+	background: #fff;
+	border-radius: 10px;
+	box-shadow: 15px 20px 0px rgba(0, 0, 0, 0.1);
+}
+
+.carousel-inner {
+	border-radius: 0 10px 10px 0;
+}
+
+.carousel-caption {
+	text-align: left;
+	left: 5%;
+}
+
+.login-sec {
+	padding: 50px 30px;
+	position: relative;
+}
+
+.login-sec .copy-text {
+	position: absolute;
+	width: 80%;
+	bottom: 20px;
+	font-size: 13px;
+	text-align: center;
+}
+
+.login-sec .copy-text i {
+	color: #FEB58A;
+}
+
+.login-sec .copy-text a {
+	color: #E36262;
+}
+
+.login-sec h2 {
+	margin-bottom: 30px;
+	font-weight: 800;
+	font-size: 30px;
+	color: #DE6262;
+}
+
+.login-sec h2:after {
+	content: " ";
+	width: 100px;
+	height: 5px;
+	background: #FEB58A;
+	display: block;
+	margin-top: 20px;
+	border-radius: 3px;
+	margin-left: auto;
+	margin-right: auto
+}
+
+.btn-login {
+	background: #DE6262;
+	color: #fff;
+	font-weight: 600;
+}
+
+.banner-text {
+	width: 70%;
+	position: absolute;
+	bottom: 40px;
+	padding-left: 20px;
+}
+
+.banner-text h2 {
+	color: #fff;
+	font-weight: 600;
+}
+
+.banner-text h2:after {
+	content: " ";
+	width: 100px;
+	height: 5px;
+	background: #FFF;
+	display: block;
+	margin-top: 20px;
+	border-radius: 3px;
+}
+
+.banner-text p {
+	color: #fff;
+}
+
+
+    
+    .reg-acc a{
+      color: black;
+          display: block;
+        margin: 0 auto;
+        text-align: center;
+       margin-top:2rem;
+        text-decoration: none;
+    }
+
+/*USER LOGIN PAGE CSS END*/
+    </style>
+
+
+
+    
+    
+    
+  <section class="login-block">
+    <div class="container">
+	<div class="row">
+		<div class="col-md-4 text-center login-sec" style="display:block; margin:0 auto;">
+		    <h2 class="text-center">User Login</h2>
+		      <form action="form_submit/user_login.php" method="post">
+  <div class="form-group">
+    <label for="exampleInputEmail1" class="text-uppercase">Username</label>
+    <input type="email" name="email" class="form-control" placeholder="">
+    
+  </div>
+  <div class="form-group">
+    <label for="exampleInputPassword1" class="text-uppercase">Password</label>
+    <input type="password" name="password" class="form-control" placeholder="">
+  </div>
+  
+  
+    <div class="form-check">
+    <label class="form-check-label">
+      <input type="checkbox" class="form-check-input">
+      <small>Remember Me</small>
+        
+    </label>
+     </div><br>
+    <button type="form_submit" class="btn btn-login text-center">login</button>
+ 
+  
+  <div class="reg-acc">
+     <a href="register.php">Register now!</a>
+  </div>
+</form>
+
+		</div>
+	
+</div>
+</section>  
+
+
 </html>
